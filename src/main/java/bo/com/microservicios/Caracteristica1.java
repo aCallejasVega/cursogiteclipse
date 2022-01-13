cd9427818cac32b4d65ea053549f439b9a01cbce
aCallejas <adalidvega@gmail.com>
2022-01-13 16:09:59 -0700

    commit caracteristica 1

---
+++ src/main/java/bo/com/microservicios/Caracteristica1.java
@@ -0,0 +1,11 @@
+package bo.com.microservicios;
+
+public class Caracteristica1 {
+
+	public static void main(String[] args) {
+		System.out.println("Caracteristica 1");
+		System.out.println("Caracteristica 1");
+		System.out.println("Caracteristica 1");
+		System.out.println("Caracteristica 1");
+	}
+}
