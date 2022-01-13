1465310cca33d8a4b5d8a0ff8189b705669a7667
DESKTOP-46AUJA3\aCallejas <adalidvega@gmail.com>
2022-01-13 15:40:36 -0700

    commit quijote

---
+++ src/main/java/bo/com/microservicios/Quijote.java
@@ -0,0 +1,11 @@
+package bo.com.microservicios;
+
+public class Quijote {
+	public static void main(String[] args) {
+		System.out.println("8888888888888888888888888888888");
+		
+		System.out.println("sdfajsdljflasjdl;jflasjlkdfjla;sdjfl;kasjd;lfkj");
+
+		System.out.println("0000000000000000000000000000000");
+	}
+}
