6d38296690453131abd7f637aa81d2ce27a05b8c
DESKTOP-46AUJA3\aCallejas <adalidvega@gmail.com>
2022-01-13 15:42:21 -0700

    hola

--- src/main/java/bo/com/microservicios/Quijote.java
+++ src/main/java/bo/com/microservicios/Quijote.java
@@ -2,6 +2,8 @@ package bo.com.microservicios;
 
 public class Quijote {
 	public static void main(String[] args) {
+		System.out.println("OOOOOOOOOOOOOOOOOOOOOOOOOOOOO");
+		
 		System.out.println("8888888888888888888888888888888");
 		
 		System.out.println("sdfajsdljflasjdl;jflasjlkdfjla;sdjfl;kasjd;lfkj");
