fa47d41d53b41f22a41a83e075610824932d85f1
aCallejas <adalidvega@gmail.com>
2022-01-13 15:58:25 -0700

    cambio que han pedido como hostfix

--- src/main/java/bo/com/microservicios/Contenido.java
+++ src/main/java/bo/com/microservicios/Contenido.java
@@ -3,6 +3,9 @@ package bo.com.microservicios;
 public class Contenido {
 	public static void main(String[] args) {
 		System.out.println("hola mundo");
+		System.out.println("hola mundo");
+		System.out.println("hola mundo");
+		System.out.println("hola mundo");
 	}
 
 }
