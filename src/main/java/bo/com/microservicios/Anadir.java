e173437a014fbf25b3b7c49865c2b03fb6e30e06
aCallejasVega <adalidvega@gmail.com>
2022-01-17 12:09:35 -0700

    segundo commit al repositorio remoto

--- src/main/java/bo/com/microservicios/Anadir.java
+++ src/main/java/bo/com/microservicios/Anadir.java
@@ -3,6 +3,9 @@ package bo.com.microservicios;
 public class Anadir {
 	public static void main(String[] args) {
 		System.out.println("hola mundo esto es para el servidor");
+		System.out.println("hola mundo esto es para el servidor");
+		System.out.println("hola mundo esto es para el servidor");
+		
 		
 	}
 
